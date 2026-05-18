00e8a428e3ce4de9496f64850c539267595e7a24
ttheomattheo <ttheo.mattheo7@gmail.com>
2026-05-18 19:49:43 +0200

    fix: bialy ekran mobile - useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { supabase } from "./supabase";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
